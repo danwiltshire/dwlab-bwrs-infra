716e663c831bc93294bc0d5db525d5746ff2a71c
danwiltshire <35703524+danwiltshire@users.noreply.github.com>
2021-03-19 22:46:01 +0000

    cleanups

--- deploy.sh
+++ deploy.sh
@@ -17,23 +17,30 @@
 CFN_TEMPLATE="infrastructure.yml"
 APPLICATION="bwrs"
 ENVIRONMENT="$1"
+VPCID="$2"
+SUBNETIDS="$3"
+ALLOWED_TRAFFIC_CIDR_IP="$4"
 
 function log_usage {
-    echo "Usage: ./deploy.sh [prod/dev/etc]"
+    echo "Usage: ./deploy.sh [prod/dev/etc] vpc-a123baa3 subnet-123a351e,subnet-123a351b,subnet-123a351c 1.123.123.3/32"
 }
 
 function lint {
-    cfn-lint "$1"
+    cfn-lint "$CFN_TEMPLATE"
 }
 
 function deploy {
     aws cloudformation deploy \
-        --template-file "$1" \
-        --stack-name "$APPLICATION-$2" \
-        --profile $2 \
+        --template-file "$CFN_TEMPLATE" \
+        --stack-name "$APPLICATION-$ENVIRONMENT" \
+        --profile $ENVIRONMENT \
+        --capabilities CAPABILITY_IAM \
         --parameter-overrides \
           Application=$APPLICATION \
-          Environment=$2
+          Environment=$ENVIRONMENT \
+          VPCID=$VPCID \
+          SubnetIDs=$SUBNETIDS \
+          AllowedTrafficCidrIp=$ALLOWED_TRAFFIC_CIDR_IP
 }
 
 # Ensure an enviroment is specified
@@ -42,5 +49,23 @@ if [ -z "$ENVIRONMENT" ]; then
   log_usage
   exit 1
 fi
+# Ensure a VPC ID is specified
+if [ -z "$VPCID" ]; then
+  echo "VPCID not supplied!"
+  log_usage
+  exit 1
+fi
+# Ensure subnet IDs are specified
+if [ -z "$SUBNETIDS" ]; then
+  echo "SUBNETIDS (comma-separated) not supplied!"
+  log_usage
+  exit 1
+fi
+# Ensure allowed traffic Cidr IP is specified
+if [ -z "$ALLOWED_TRAFFIC_CIDR_IP" ]; then
+  echo "ALLOWED_TRAFFIC_CIDR_IP not supplied!"
+  log_usage
+  exit 1
+fi
 
-lint "$CFN_TEMPLATE" && deploy "$CFN_TEMPLATE" "$ENVIRONMENT"
+lint && deploy
